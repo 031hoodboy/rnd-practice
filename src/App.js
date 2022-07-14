7b74dd80391101062cb92a102afd0a55a54df10e
031hoodboy <littlepark0107@gamil.com>
2022-07-14 13:03:15 +0900

    feat: devide header component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useRef } from "react";
 import { Rnd } from "react-rnd";
 import styled from "styled-components";
 import uuid from "react-uuid";
-import { ReactComponent as LogoIcon } from "./assets/logo.svg";
-import { ReactComponent as SearchIcon } from "./assets/search.svg";
+import Header from "./Components/Header";
+
 import "./styles.css";
 
 // import TextEditor from "./Components/TextEditor";
@@ -101,24 +101,7 @@ const App = () => {
 
   return (
     <PageBlock>
-      <Header>
-        <LogoBlock>
-          <LogoIcon />
-        </LogoBlock>
-        <SearchbarBlock>
-          <SearchbarWrapper>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <Searchinput placeholder="주소를 입력해주세요. (예시. 지번주소)" />
-          </SearchbarWrapper>
-        </SearchbarBlock>
-        <Sidebar>
-          <Nav>리포트</Nav>
-          <Nav>분석</Nav>
-          <Nav>데이터 다운로드</Nav>
-        </Sidebar>
-      </Header>
+      <Header />
       <MapEditBlock>
         <AddBlockWrapper>
           <AddBlock onClick={onCreateBlock}>Add Block</AddBlock>
@@ -213,77 +196,6 @@ const PageBlock = styled.div`
   padding-top: 65px;
 `;
 
-const Header = styled.div`
-  width: 100%;
-  height: 65px;
-  display: flex;
-  align-items: center;
-  position: fixed;
-  top: 0;
-`;
-
-const LogoBlock = styled.div`
-  width: 258px;
-  height: 65px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #fff;
-`;
-
-const SearchbarBlock = styled.div`
-  width: calc(100% - 258px);
-  height: 100%;
-  background-color: #fff;
-  display: flex;
-  align-items: center;
-`;
-
-const Sidebar = styled.div`
-  width: 258px;
-  height: 100%;
-  background-color: #fff;
-  position: fixed;
-  top: 65px;
-  left: 0;
-`;
-
-const Nav = styled.div`
-  display: flex;
-  font-size: 13px;
-  padding: 12px 28px 12px 32px;
-`;
-
-const SearchbarWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  padding-left: 12px;
-`;
-
-const SearchIconWrapper = styled.div`
-  position: absolute;
-  margin-left: 12px;
-  svg {
-    margin-top: 4px;
-    width: 22px;
-    height: 22px;
-  }
-`;
-
-const Searchinput = styled.input`
-  width: 458px;
-  font-size: 13px;
-  padding: 10px 10px 10px 48px;
-  outline: none;
-  border: none;
-  font-weight: 400;
-  line-height: 1.4375em;
-  color: rgba(0, 0, 0, 0.87);
-  &:hover {
-    background-color: #f4f4f5;
-  }
-`;
-
 const Map = styled.img`
   min-width: 1247px;
   height: 800px;
@@ -315,9 +227,14 @@ const CircleBox = styled(Box)`
 `;
 
 const MapEditBlock = styled.div`
-  padding-left: 288px;
+  width: 1247px;
   margin: 0 auto;
-  min-width: 1247px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 65px;
+  left: 288px;
 `;
 
 const AddBlockWrapper = styled.div`
